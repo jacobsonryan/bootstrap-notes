8515a1964202135acb4cd157a8ee4ea78c32e3ec
Ryan Jacobson <ryan@Ryans-MBP.home>
2020-08-06 19:49:40 -0400

    Updates

---
+++ precache-manifest.dfb352c1c5a490f3a4727ea7f16adca3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c088f734640090dab745856209094231",
+    "url": "/bootstrap-notes/index.html"
+  },
+  {
+    "revision": "6191e24c951bbb531718",
+    "url": "/bootstrap-notes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cadbfb2c091f2b764a43",
+    "url": "/bootstrap-notes/static/css/main.72bdda30.chunk.css"
+  },
+  {
+    "revision": "6191e24c951bbb531718",
+    "url": "/bootstrap-notes/static/js/2.fa2824c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bootstrap-notes/static/js/2.fa2824c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadbfb2c091f2b764a43",
+    "url": "/bootstrap-notes/static/js/main.78ffedcc.chunk.js"
+  },
+  {
+    "revision": "372610979e383dc25fb5",
+    "url": "/bootstrap-notes/static/js/runtime-main.d06d97ae.js"
+  }
+]);
